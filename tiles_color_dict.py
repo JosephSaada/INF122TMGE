70215c58a3575a8d930a7141bf07e2a066cba705
IncentWang <wwhincent@gmail.com>
2024-03-17 16:02:38 -0700

    Added Candy Crush special Tiles

--- tiles_color_dict.py
+++ tiles_color_dict.py
@@ -1,6 +1,7 @@
 class TilesColorDict:
     def __init__(self):
         self.tiles_color_dict = {"red": "red", "blue": "blue", "green": "green", "yellow": "yellow", "orange": "orange", "purple": "purple", "pink": "pink", "empty": "white"} # for more tiles and how to display them, add their names and corresponding colors here
-    
+        self.tiles_color_dict["special_4_connected"] = "black"
+        self.tiles_color_dict["special_5_connected"] = "cyan"
     def get_color(self, tileType):
         return self.tiles_color_dict[tileType]
