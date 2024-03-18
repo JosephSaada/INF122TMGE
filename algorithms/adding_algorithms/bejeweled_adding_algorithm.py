ad119e9f7d3bf20861c8db59f2ff20b37970e9f9
Kyle Tran <48851847+kyletran77@users.noreply.github.com>
2024-03-18 10:06:38 -0700

    fix black special tiles, and vertical matches

--- algorithms/adding_algorithms/bejeweled_adding_algorithm.py
+++ algorithms/adding_algorithms/bejeweled_adding_algorithm.py
@@ -1,5 +1,5 @@
 import random
-from tiles.bejeweled_tile import BejeweledTile  # Assuming BejeweledTile is in a module named tiles
+from tiles.bejeweled_tile import BejeweledTile  
 from algorithms.adding_algorithms.base_adding_algorithm import BaseAddingAlgorithm
 
 class BejeweledAddingAlgorithm(BaseAddingAlgorithm):
@@ -7,11 +7,12 @@ class BejeweledAddingAlgorithm(BaseAddingAlgorithm):
         for col in range(board.col):
             for row in range(board.row):
                 if board.board[row][col].type == "empty":
-                    tileType = random.choice(board.tilesSet)
-                    is_special = self.should_be_special()  # You define this method
+                    filtered_tileset = [tile for tile in board.tilesSet if tile != "black"]
+                    tileType = random.choice(filtered_tileset)
+                    is_special = self.should_be_special()  
                     board.board[row][col] = BejeweledTile(row, col, tileType, is_special)
-        return super().add(board)  # Optionally use the base method for additional logic
+        return super().add(board)  
 
     def should_be_special(self):
-        # Example condition for creating a special tile
-        return random.random() < 0.1  # 10% chance
+        #  condition for creating a special black tile to spawn
+        return random.random() < 0.001  
